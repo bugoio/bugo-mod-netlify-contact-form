f5cf3eebbd73bb715b6be82382ecc7a326115cbf
Matt Antone <matt.antone@gmai.com>
2019-09-04 14:03:31 -0700

    testing form

--- assets/js/netlify-contact-form.js
+++ assets/js/netlify-contact-form.js
@@ -1,8 +1,11 @@
-$("#netlify-contact-form").submit(function(e) {
-  e.preventDefault();
-
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thank you!");
+$(document).ready(function(e){
+  $("#netlify-contact-form").submit(function(e) {
+    e.preventDefault();
+    var $form = $(this);
+    console.log($form);
+    // $.post($form.attr("action"), $form.serialize()).then(function() {
+    //   alert("Thank you!");
+    // });
   });
 });
+
